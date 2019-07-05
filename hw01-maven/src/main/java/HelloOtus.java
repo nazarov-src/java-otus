63c9a0306e7db8135ff21c6793e032d63969acc5
Sergey Nazarov <noncopyable@yandex.ru>
2019-07-05 03:32:28 +0300

    Guava usage added

---
+++ hw01-maven/src/main/java/HelloOtus.java
@@ -0,0 +1,8 @@
+import com.google.common.collect.ImmutableList;
+
+public class HelloOtus {
+    public static void main(String[] args) {
+        ImmutableList<Integer> list = ImmutableList.of(1,2,3);
+        System.out.println(list);
+    }
+}
